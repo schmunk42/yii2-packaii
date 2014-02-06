13c7c580203fc9bc02856fe403b5386649bc9fc6
Tobias Munk <schmunk@usrbin.de>
2014-02-06 03:34:45 +0100

    cf

--- Module.php
+++ Module.php
@@ -5,12 +5,12 @@ namespace schmunk42\packageBrowser;
 
 class Module extends \yii\base\Module
 {
-	public $controllerNamespace = 'schmunk42\packageBrowser\controllers';
+    public $controllerNamespace = 'schmunk42\packageBrowser\controllers';
 
-	public function init()
-	{
-		parent::init();
+    public function init()
+    {
+        parent::init();
 
-		// custom initialization code goes here
-	}
+        // custom initialization code goes here
+    }
 }
