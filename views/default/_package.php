6d04b58ecf46a9147c77ac125317146f667932f0
Tobias Munk <schmunk@usrbin.de>
2014-03-03 20:28:06 +0100

    minor UI fix

--- views/default/_package.php
+++ views/default/_package.php
@@ -7,14 +7,12 @@ $version = property_exists($model, 'version') ? $model->version : null;
 <h5>
 	<?php if (Yii::$app->getModule('packaii')->manager->getIsInstalled($model->name)): ?>
 		<span
-			class="label label-<?= isset($model->isRequired) ? 'success' : 'primary' ?>">
-			<?= (isset($model->version)) ? $model->version : 'installed' ?>
-		</span>
+			class="label label-<?= isset($model->isRequired) ? 'success' : 'primary' ?>"><?= (isset($model->version)) ? $model->version : 'installed' ?></span>
 	<?php else: ?>
 		<span class="label label-default">not installed</span>
 	<?php endif; ?>
 	<?php if (isset($model->isRequiredDev)): ?>
-		<span class="label label-important">dev</span>
+		<span class="label label-warning">dev</span>
 	<?php endif; ?>
 	<?=
 	Html::a($model->name, Html::url([
