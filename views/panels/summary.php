76e5ed891a9519586bcb6a09c195bfefaae16afe
Tobias Munk <schmunk@usrbin.de>
2014-03-21 19:08:33 +0100

    fixes due to Yii 2 API changes

--- views/panels/summary.php
+++ views/panels/summary.php
@@ -1,9 +1,9 @@
 <?php
-use \yii\helpers\Html;
+use \yii\helpers\Url;
 
 ?>
 <div class="yii-debug-toolbar-block">
-	<a href="<?= Html::url(['/packaii']) ?>" class="label label-important">
+	<a href="<?= Url::to(['/packaii']) ?>" class="label label-important">
 		<span class="glyphicon glyphicon-gift"></span>
 		Packaii
 	</a>
